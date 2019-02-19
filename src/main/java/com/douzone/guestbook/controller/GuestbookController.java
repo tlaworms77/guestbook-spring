b6b5e971ae981cf5b668f833b5b5f61147d26e74
tlawo <tlawo@LAPTOP-ICSS4D7B>
2019-02-19 09:56:14 +0900

    refactoring... ( release 1.0.1 )

--- src/main/java/com/douzone/guestbook/controller/GuestbookController.java
+++ src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -3,8 +3,10 @@ package com.douzone.guestbook.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.douzone.guestbook.dao.GuestBookDao;
@@ -14,31 +16,30 @@ import com.douzone.guestbook.vo.GuestBookVo;
 public class GuestbookController {
 	@Autowired
 	private GuestBookDao guestbookDao;
-	
-	@RequestMapping({"", "index"})
+
+	@RequestMapping("")
 	public String index(Model model) {
 		model.addAttribute("list", guestbookDao.getList());
 		return "/WEB-INF/views/index.jsp";
 	}
-	
-	@RequestMapping("deleteform/{no}")
-	public String deleteform( Model model,
-		@PathVariable("no") Long no) {
-		
+
+	@RequestMapping(value = "deleteform/{no}", method = RequestMethod.GET)
+	public String deleteform(Model model, @PathVariable("no") Long no) {
+
 		model.addAttribute("no", no);
 		return "/WEB-INF/views/deleteform.jsp";
 	}
-	
-	@RequestMapping("delete")
-	public String delete(GuestBookVo vo) {
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public String delete(@ModelAttribute GuestBookVo vo) {
 		guestbookDao.delete(vo);
 		return "redirect:/";
 	}
-	
-	@RequestMapping("add")
-	public String add(GuestBookVo vo) {
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(@ModelAttribute GuestBookVo vo) {
 		guestbookDao.insert(vo);
 		return "redirect:/";
 	}
-	
+
 }
